ce7684fefeeaba097c44e11ecbe51a1906182251
JulietaSegoviaRaices <puntoesenciacrochet@gmail.com>
2026-07-22 22:57:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -188,37 +188,41 @@ mostrar();
 
 function guardar(){
 
-localStorage.setItem(
-"plantas",
-JSON.stringify(plantas)
-);
+try{
+
+    localStorage.setItem(
+        "plantas",
+        JSON.stringify(plantas)
+    );
 
 
-plantas.forEach(function(planta){
+    plantas.forEach(function(planta){
 
-guardarPlantaDB(planta)
-.then(()=>{
+        guardarPlantaDB(planta)
+        .then(()=>{
 
-console.log("Guardada en IndexedDB");
+            console.log("Guardada en IndexedDB");
 
-})
-.catch(error=>{
+        })
+        .catch(error=>{
 
-console.error(error);
+            console.error(error);
 
-});
+        });
 
-});
+    });
 
 
-}
-console.log("Guardado correcto");
+    console.log("Guardado correcto");
+
 
 }catch(error){
 
-console.error(error);
+    console.error(error);
 
-alert("Error guardando. La foto es demasiado grande");
+    alert("Error guardando. La foto es demasiado grande");
+
+}
 
 }
 
